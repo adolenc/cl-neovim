50d8b2e00003764ef6069bf37e628fdf47f191c7
adolenc <andrej.dolenc@student.uni-lj.si>
2016-06-24 16:54:45 +0200

    Redirect errors that reach host to log output

--- autoload/host.lisp
+++ autoload/host.lisp
@@ -51,8 +51,9 @@
 
 (setf nvim::*using-host* T
       nvim::*log-stream* (make-broadcast-stream))
-(handler-case (mrpc::run-forever (mrpc::event-loop nvim::*nvim-instance*))
-  (T (desc)
-    (format nvim::*log-stream* "Event loop received error:~%~A~%Closing lisp host.~%" desc) 
-    (force-output nvim::*log-stream*)
-    (close nvim::*log-stream*)))
+(nvim::redirect-output (nvim::*log-stream*)
+  (handler-case (mrpc::run-forever (mrpc::event-loop nvim::*nvim-instance*))
+    (error (desc)
+      (format nvim::*log-stream* "Event loop received error:~%~A~%Closing lisp host.~%" desc) 
+      (force-output nvim::*log-stream*)
+      (close nvim::*log-stream*))))
