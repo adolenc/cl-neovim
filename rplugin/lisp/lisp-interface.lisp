44c0a1c639d0ef5de14b6911ba77500db7d471ad
adolenc <andrej.dolenc@student.uni-lj.si>
2016-07-02 00:17:06 +0200

    Add :Lispdo command (lisp equivalent of :pydo)

--- rplugin/lisp/lisp-interface.lisp
+++ rplugin/lisp/lisp-interface.lisp
@@ -20,5 +20,24 @@
   (declare (opts nargs))
   (echo-output (eval-string (format nil "~{~A~^ ~}" form))))
 
+(nvim:defcommand/s lispdo (&rest form &opts range)
+  (declare (opts nargs range))
+  (let ((fn (eval-string
+              (format nil "#'(lambda (line line-nr)
+                               (declare (ignorable line line-nr))
+                               ~{~A~^ ~})"
+                      form)))
+        (start (1- (first range)))
+        (end (second range)))
+    (echo-output
+      (let ((new-lines (mapcar #'(lambda (line line-nr)
+                                   (or (let ((new-line (funcall fn line line-nr)))
+                                         (if (or (stringp new-line) (null new-line))
+                                           new-line
+                                           (error "Function must return either string or NIL.")))
+                                       line))
+                               (nvim:buffer-lines (nvim:current-buffer) start end T)
+                               (loop for line-nr from start upto end collect (1+ line-nr)))))
+        (setf (nvim:buffer-lines (nvim:current-buffer) start end T) new-lines)))))
 
 (in-package :cl-user)
