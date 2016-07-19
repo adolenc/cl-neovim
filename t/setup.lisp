bcd2a6e7d2b48f8a669675c552265228c720f3c6
adolenc <andrej.dolenc@student.uni-lj.si>
2016-07-19 20:17:37 +0200

    Implement clean-up fixture for api tests
    Mostly copied directly from official python-client.

--- t/setup.lisp
+++ t/setup.lisp
@@ -23,5 +23,53 @@
      ,@body
      (loop for result = (get-result-from-nvim) then (get-result-from-nvim)
            while (and (stringp result) (string= result "result not set"))
-           do (sleep 0.05)
+           do (sleep 0.5)
            finally (return result))))
+
+(defparameter *cleanup*
+":function BeforeEachTest()
+    set all&
+    redir => groups
+    silent augroup
+    redir END
+    for group in split(groups)
+      exe 'augroup '.group
+      autocmd!
+      augroup END
+    endfor
+    autocmd!
+    tabnew
+    let curbufnum = eval(bufnr('%'))
+    redir => buflist
+    silent ls!
+    redir END
+    let bufnums = []
+    for buf in split(buflist, '\\n')
+      let bufnum = eval(split(buf, '[ u]')[0])
+      if bufnum != curbufnum
+        call add(bufnums, bufnum)
+      endif
+    endfor
+    if len(bufnums) > 0
+      exe 'silent bwipeout! '.join(bufnums, ' ')
+    endif
+    silent tabonly
+    for k in keys(g:)
+      exe 'unlet g:'.k
+    endfor
+    filetype plugin indent off
+    mapclear
+    mapclear!
+    abclear
+    comclear
+  endfunction
+")
+
+(def-fixture cleanup ()
+  (when (= 0 (nvim:call/s t "vim_call_function" "exists" '("*BeforeEachTest")))
+    (nvim:call/s t "vim_input" *cleanup*))
+  (nvim:call/s t "vim_call_function" "BeforeEachTest" #())
+  (is (= 1 (length (nvim:call/s t "vim_get_tabpages"))))
+  (is (= 1 (length (nvim:call/s t "vim_get_windows"))))
+  (is (= 1 (length (nvim:call/s t "vim_get_buffers"))))
+  (&body))
