0c95421ea3377c4f8fd2c3822afe63a17fff5cb8
adolenc <andrej.dolenc@student.uni-lj.si>
2016-12-07 22:55:10 +0100

    Move code around to more sensible locations

---
+++ src/manual-api.lisp
@@ -0,0 +1,37 @@
+(in-package #:cl-neovim)
+
+
+(cl:defun buffer-number (buffer &optional (instance *nvim-instance*))
+  (declare (ignore instance))
+  (mpk::decode (mpk::extension-type-id buffer)))
+
+(cl:defun buffer-number/a (buffer &optional (instance *nvim-instance*))
+  (declare (ignore instance))
+  (mpk::decode (mpk::extension-type-id buffer)))
+
+(cl:defun subscribe (event function &optional (instance *nvim-instance*))
+  (mrpc:register-callback instance event function)
+  (nvim:call/s instance "vim_subscribe" event))
+
+(cl:defun subscribe/a (event function &optional (instance *nvim-instance*))
+  (mrpc:register-callback instance event function)
+  (nvim:call/a instance "vim_subscribe" event))
+
+(cl:defun unsubscribe (event &optional (instance *nvim-instance*))
+  (nvim:call/s instance "vim_unsubscribe" event)
+  (mrpc:remove-callback instance event))
+
+(cl:defun unsubscribe/a (event &optional (instance *nvim-instance*))
+  (nvim:call/a instance "vim_unsubscribe" event)
+  (mrpc:remove-callback instance event))
+
+(defmacro call-atomic ((&optional (instance *nvim-instance*)) &rest body)
+  `(destructuring-bind (results err)
+                       (nvim:call/s ,instance "nvim_call_atomic"
+                                    (let ((*should-capture-calls* T)
+                                          (*captured-calls* (list)))
+                                      ,@body
+                                      (reverse *captured-calls*)))
+     (if err
+       (error 'mrpc:rpc-error :message err)
+       results)))
